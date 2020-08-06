e57ce5c5e4e670cbb3f3cbfade30b98a21c59279
KarthikaRagu <karthikaragu@gmail.com>
2020-08-06 22:17:39 +0530

    Commit - 6/8

---
+++ src/test/java/com/scm/order/processing/MsOrderProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.order.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOrderProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
